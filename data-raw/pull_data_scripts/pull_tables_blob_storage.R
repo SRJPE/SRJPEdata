3b72bf9b5562c27586a855db6ff9eba556b2ffcc
Badhia <byuneskatz@flowwest.com>
2024-08-08 12:58:24 -0700

    adds script placeholder for adding rst data

---
+++ data-raw/pull_data_scripts/pull_tables_blob_storage.R
@@ -0,0 +1,23 @@
+
+library(DBI)
+library(tidyverse)
+library(lubridate)
+library(SRJPEdata)
+
+#Connect to data cloud TBD
+
+#pull in tables
+
+#catch
+# usethis::use_data(rst_catch_nosrjpe, overwrite = TRUE)
+
+#recapture
+# usethis::use_data(rst_recapture_nosrjpe, overwrite = TRUE)
+
+#release
+# usethis::use_data(rst_release_nosrjpe, overwrite = TRUE)
+
+#trap
+# usethis::use_data(rst_trap_nosrjpe, overwrite = TRUE)
+
+
