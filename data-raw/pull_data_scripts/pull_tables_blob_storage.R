ccc6d0094c3de226893fa18ad632026911c2d322
Badhia <byuneskatz@flowwest.com>
2024-08-28 11:11:33 -0700

    overwrites nosrjpe tables with fixed combined data from AWS bucket

--- data-raw/pull_data_scripts/pull_tables_blob_storage.R
+++ data-raw/pull_data_scripts/pull_tables_blob_storage.R
@@ -4,7 +4,7 @@ library(lubridate)
 library(SRJPEdata)
 library("pins")
 library(dplyr)
-config::get(file = "config.yml")
+# config::get(file = "config.yml")
 access_key <- Sys.getenv("aws_access_key_id")
 secret_access_key <- Sys.getenv("secret_access_key_id")
 session_token <- Sys.getenv("session_token_id")
@@ -24,16 +24,20 @@ pins::pin_list(hrl_project_board)
 #pull in tables
 
 #catch
-rst_catch_nosrjpe <- pins::pin_read(hrl_project_board, "catch")
+rst_catch_nosrjpe <- pins::pin_read(hrl_project_board, "catch") |> 
+  glimpse()
 
 #recapture
-rst_recapture_nosrjpe <- pins::pin_read(hrl_project_board, "recapture")
+rst_recapture_nosrjpe <- pins::pin_read(hrl_project_board, "recapture") |> 
+  glimpse()
 
 #release
-rst_release_nosrjpe <- pins::pin_read(hrl_project_board, "release")
+rst_release_nosrjpe <- pins::pin_read(hrl_project_board, "release") |> 
+  glimpse()
 
 #trap
-rst_trap_nosrjpe <- pins::pin_read(hrl_project_board, "trap")
+rst_trap_nosrjpe <- pins::pin_read(hrl_project_board, "trap") |> #this table is empty since no trap data associated with Mokelumne or American River
+  glimpse()
 
 
 #save data to package
