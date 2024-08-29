49b527113630042b000acece21c15403b0309665
Erin Cain <ecain@flowwest.com>
2024-08-29 08:54:37 -0700

    Adds basic tests for adult data pulls

--- tests/testthat/tests_database_pulls.R
+++ tests/testthat/tests_database_pulls.R
@@ -48,6 +48,110 @@ test_that("Always one non NA value in trap date variables", {
 }) 
 
 # Efficiency data tests 
+# stream consistant
+test_that("RST release data coverage at the tributary level has not changed", {
+  current_coverage <- release$stream |> unique() |> sort()
+  
+  #TODO we seem to be missing mill and deer release data
+  expected_coverage <- c("battle creek", "butte creek", "clear creek", "deer creek", 
+                         "feather river", "mill creek", "sacramento river")
+  expect_equal(current_coverage, expected_coverage)
+})
+
+test_that("No NA values in identifying release data variables", {
+  expect_equal(any(is.na(release$stream)), FALSE)
+  expect_equal(any(is.na(release$site)), FALSE)
+  expect_equal(any(is.na(release$subsite)), FALSE) # TODO there should not be NAs here right? Or maybe this is fine
+  expect_equal(any(is.na(release$site_group)), FALSE)
+  expect_equal(any(is.na(release$date_released)), FALSE)
+  
+})
+
+test_that("RST recapture data coverage at the tributary level has not changed", {
+  current_coverage <- recaptures$stream |> unique() |> sort()
+  
+  expected_coverage <- c("battle creek", "butte creek", "clear creek", "deer creek", 
+                         "feather river", "mill creek", "sacramento river")
+  expect_equal(current_coverage, expected_coverage)
+})
+
+test_that("No NA values in identifying recapture data variables", {
+  expect_equal(any(is.na(recaptures$stream)), FALSE)
+  expect_equal(any(is.na(recaptures$site)), FALSE)
+  expect_equal(any(is.na(recaptures$subsite)), FALSE) 
+  expect_equal(any(is.na(recaptures$site_group)), FALSE) # TODO there should not be NAs here right? 
+  expect_equal(any(is.na(recaptures$date)), FALSE)
+})
+
+
+# Adult data tests -------------------------------------------------------------
+# Passage Counts
+test_that("Upstream passage counts data coverage at the tributary level has not changed", {
+  current_coverage <- upstream_passage$stream |> unique() |> sort()
+  
+  expected_coverage <- c("battle creek", "clear creek", "deer creek", "mill creek", 
+                         "yuba river")
+  expect_equal(current_coverage, expected_coverage)
+})
+
+test_that("No NA values in identifying passage counts data variables", {
+  expect_equal(any(is.na(upstream_passage$stream)), FALSE)
+  expect_equal(any(is.na(upstream_passage$date)), FALSE)
+})
+
+# Passage Estimates
+test_that("Upstream passage estimates data coverage at the tributary level has not changed", {
+  current_coverage <- upstream_passage_estimates$stream |> unique() |> sort()
+  
+  expected_coverage <- c("battle creek", "clear creek", "deer creek", "mill creek", 
+                         "yuba river")
+  expect_equal(current_coverage, expected_coverage)
+})
+
+test_that("No NA values in identifying passage estimates data variables", {
+  expect_equal(any(is.na(upstream_passage_estimates$stream)), FALSE)
+  expect_equal(any(is.na(upstream_passage_estimates$year)), FALSE)
+})
+
+# Redd
+test_that("Redd data coverage at the tributary level has not changed", {
+  current_coverage <- redd$stream |> unique() |> sort()
+  
+  expected_coverage <- c("battle creek", "clear creek", "feather river", "mill creek", 
+                         "yuba river")
+  expect_equal(current_coverage, expected_coverage)
+})
+
+test_that("No NA values in identifying redd data variables", {
+  expect_equal(any(is.na(redd$stream)), FALSE)
+  expect_equal(any(is.na(redd$date)), FALSE)
+})
+
+# carcass
+test_that("Carcass estinates data coverage at the tributary level has not changed", {
+  current_coverage <- carcass_estimates$stream |> unique() |> sort()
+  
+  expected_coverage <- c("butte creek", "feather river", "yuba river")
+  expect_equal(current_coverage, expected_coverage)
+})
+
+test_that("No NA values in identifying carcass data variables", {
+  expect_equal(any(is.na(carcass_estimates$stream)), FALSE)
+  expect_equal(any(is.na(redcarcass_estimatesd$year)), FALSE)
+})
+
+# holding
+test_that("Holding data coverage at the tributary level has not changed", {
+  current_coverage <- holding$stream |> unique() |> sort()
+  
+  expected_coverage <- c("battle creek", "butte creek", "clear creek", "deer creek")
+  expect_equal(current_coverage, expected_coverage)
+})
+
+test_that("No NA values in identifying holding data variables", {
+  expect_equal(any(is.na(holding$stream)), FALSE)
+  expect_equal(any(is.na(holding$year)), FALSE)
+})
 
 
 
