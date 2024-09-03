3879eeb3626fcd6ba46487bed6e3443616c954ad
Erin Cain <ecain@flowwest.com>
2024-09-03 08:44:55 -0700

    Updates years to include analysis to fix site group issues with feather and sacramento

--- tests/testthat/tests_intermediate_data_objects.R
+++ tests/testthat/tests_intermediate_data_objects.R
@@ -2,7 +2,6 @@
 # Test that `chosen_site_years_to_model` includes all streams
 test_that("chosen_site_years_to_model includes all streams", {
   current_coverage <- chosen_site_years_to_model$stream |> unique() |> sort()
-  # TODO where is feather? 
   expected_coverage <- c("battle creek", "butte creek", "clear creek", "deer creek", 
                          "feather river", "mill creek", "sacramento river", "yuba river")
   expect_equal(current_coverage, expected_coverage)
@@ -11,8 +10,8 @@ test_that("chosen_site_years_to_model includes all streams", {
 
 test_that("chosen_site_years_to_model includes all sites", {
   current_coverage <- chosen_site_years_to_model$site |> unique() |> sort()
-  # TODO where is feather? 
-  expected_coverage <- c("adams dam", "deer creek", "eye riffle", "gateway riffle", 
+  # TODO confirm that we do not want adams dam in there at all
+  expected_coverage <- c("deer creek", "eye riffle", "gateway riffle", 
                          "hallwood", "herringer riffle", "knights landing", "lbc", "lcc", 
                          "live oak", "lower feather river", "mill creek", "okie dam", 
                          "red bluff diversion dam", "shawn's beach", "steep riffle", "sunset pumps", 
@@ -22,7 +21,6 @@ test_that("chosen_site_years_to_model includes all sites", {
 
 test_that("chosen_site_years_to_model includes all sites_groups", {
   current_coverage <- chosen_site_years_to_model$site_group |> unique() |> sort()
-  # TODO missing feather site groups
   expected_coverage <- c("battle creek", "butte creek", "clear creek", "deer creek", 
                          "knights landing", "lower feather river", "mill creek", "red bluff diversion dam", 
                          "tisdale", "upper feather hfc", "upper feather lfc", "yuba river")
