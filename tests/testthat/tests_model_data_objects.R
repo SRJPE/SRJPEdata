0f12b78e853930d7fd63d4af9eaeb7c2037aa234
alvizek <avizek@flowwest.com>
2024-12-13 10:04:52 -0800

    adds a new check with years to include

--- tests/testthat/tests_model_data_objects.R
+++ tests/testthat/tests_model_data_objects.R
@@ -15,6 +15,32 @@ test_that("weekly_juvenile_efficeincy_catch_data data coverage at the tributary 
   # TODO where is feather? 
   # Where are mill and deer
   expected_coverage <- c("battle creek", "butte creek", "clear creek", "deer creek", 
-                         "feather river", "mill creek", "sacramento river")
+                         "feather river", "mill creek", "sacramento river", "yuba river")
   expect_equal(current_coverage, expected_coverage)
 })
+
+# years to exclude are actually excluded
+test_that("weekly_juvenile_abundance_catch_data has the appropriate run years are included", {
+  current_site_year_raw <- weekly_juvenile_abundance_catch_data |> 
+    #filter(life_stage != "yearling") |> 
+    #filter(count > 0) |> 
+    distinct(stream, site, run_year) |> 
+    mutate(site_year = paste0(site, "-", run_year))
+  current_site_year <- sort(current_site_year_raw$site_year)
+  chosen_site_year_raw <- chosen_site_years_to_model |> 
+    select(monitoring_year, stream, site) |> 
+    rename(run_year = monitoring_year) |> 
+    mutate(site_year = paste0(site, "-", run_year)) |> 
+    filter(!(run_year == 2025)) # make sure to remove this next year!
+  chosen_site_year <- sort(chosen_site_year_raw$site_year)
+  expect_equal(current_site_year, chosen_site_year)
+})
+
+# test_join <- current_site_year_raw |> 
+#   rename(current_site_year = site_year) |> 
+#   full_join(chosen_site_year_raw)
+
+# no missing values where we have catch
+#test_that("weekly_juvenile_abundance_data does not have missing values where we have catch")
+# if data for one lifestage, count is not missing for others
+# we have all weeks
