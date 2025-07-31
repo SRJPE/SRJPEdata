b2f083a4d71c8263d48811ed4630b1baea0e692c
alvizek <avizek@flowwest.com>
2025-07-31 15:29:53 -0700

    adds butte fyke filter

--- data-raw/pull_data_scripts/combine_database_pull_and_save.R
+++ data-raw/pull_data_scripts/combine_database_pull_and_save.R
@@ -9,13 +9,34 @@ source("data-raw/pull_data_scripts/pull_misfit_rst_data.R") # Battle and Clear r
 # "standard-format-data/standard_recapture.csv"
 standard_release <- read_csv("data-raw/helper-tables/standard_release.csv")
 
-
 # Bind rows 
-rst_catch <- bind_rows(rst_catch, rst_catch_query_pilot, edi_catch) |> 
+rst_catch_prep <- bind_rows(rst_catch, rst_catch_query_pilot, edi_catch) |> 
   mutate(fork_length = ifelse(fork_length == 0, NA, fork_length), # looks like there were some cases where fork length is 0. this should be handled more upstream but fixing it here for now
          julian_week = week(date),
-         julian_year = year(date)) |> # adding week for noble
+         julian_year = year(date),# adding week for noble
+         life_stage = ifelse(is.na(life_stage), "not recorded", life_stage),
+         subsite = case_when(site == "okie dam" & is.na(subsite) ~ "okie dam 1", # fix missing subsites
+                             is.na(subsite) ~ site,
+                             T ~ subsite)) |> 
   glimpse()
+
+# find dates where the rst is fishing to use as a filter
+okie_rst <- rst_catch_prep |> 
+  filter(site == "okie dam", subsite != "okie dam fyke trap") |> 
+  mutate(date = as_date(date)) |> # remove time so that won't affect it
+  pull(date)
+
+rst_catch <- rst_catch_prep |> 
+  mutate(butte_fyke_filter = case_when(site == "okie dam" & as_date(date) %in% okie_rst ~ "rst & fyke",
+                                       site == "okie dam" & !as_date(date) %in% okie_rst ~ "fyke only",
+                                       T ~ "not butte")) |> 
+  # some of these happen due to EDI join and the datatackle include more species than chinook
+  filter(species %in% c("chinook","chinook salmon"), # filter for only chinook
+         life_stage != "adult",# remove the adult fish (mostly on Butte)
+         !is.na(stream),# 7 NAs I think come from knights landings
+         butte_fyke_filter != "fyke only") |> 
+  select(-c(butte_fyke_filter))
+
 rst_trap <- bind_rows(rst_trap, rst_trap_query_pilot, edi_trap)  |> glimpse()
 release <- bind_rows(release_db, release_query_pilot |> 
                        mutate(release_id = as.character(release_id)), edi_release)  |> 
