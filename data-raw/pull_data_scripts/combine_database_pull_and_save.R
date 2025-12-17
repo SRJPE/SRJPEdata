cb6d591bca2ab89b1ad16b2f3f5bdde322bcdecc
alvizek <avizek@flowwest.com>
2025-12-17 13:52:21 -0800

    updates datatackle trap query and adds dates when no salmon caught but trap fishing

--- data-raw/pull_data_scripts/combine_database_pull_and_save.R
+++ data-raw/pull_data_scripts/combine_database_pull_and_save.R
@@ -1,5 +1,5 @@
 library(tidyverse)
-# Bind together tables from SRJPE data and data tackle 
+# Bind together tables from SRJPE data and data tackle
 source("data-raw/pull_data_scripts/pull_tables_from_database.R")
 source("data-raw/pull_data_scripts/pull_tables_from_data_tackle_database.R")
 source("data-raw/pull_data_scripts/pull_misfit_rst_data.R") # Battle and Clear recapture, KNL pre 2004, Butte pre 2015
@@ -9,50 +9,148 @@ source("data-raw/pull_data_scripts/pull_misfit_rst_data.R") # Battle and Clear r
 # "standard-format-data/standard_recapture.csv"
 standard_release <- read_csv("data-raw/helper-tables/standard_release.csv")
 
-# Bind rows 
-rst_catch_prep <- bind_rows(rst_catch, rst_catch_query_pilot, edi_catch) |> 
-  mutate(fork_length = ifelse(fork_length == 0, NA, fork_length), # looks like there were some cases where fork length is 0. this should be handled more upstream but fixing it here for now
-         julian_week = week(date),
-         julian_year = year(date),# adding week for noble
-         life_stage = ifelse(is.na(life_stage), "not recorded", life_stage),
-         subsite = case_when(site == "okie dam" & is.na(subsite) ~ "okie dam 1", # fix missing subsites
-                             is.na(subsite) ~ site,
-                             subsite == "yub" ~ "hal", # we made the decision to include these historical ones as halwood
-                             T ~ subsite)) |> 
-  glimpse()
+# rst_trap ----------------------------------------------------------------
 
+# processing code to assign the trap_visit_time_start for data from datatackle
+rst_trap_query_pilot_processed <- rst_trap_query_pilot |>
+  filter(trap_visit_time_end > as_date("2023-09-01")) |> # removes the entries from Jan 2023 (those go in previous season)
+  arrange(trap_name, trap_visit_time_end) |>
+  mutate(
+    trap_visit_time_restart = case_when(
+      is.na(trap_visit_time_restart) ~ trap_visit_time_end,
+      T ~ trap_visit_time_restart
+    ),
+    # note that this is not perfect - ideally there is a trapvisit for when they restart the trap
+    trap_visit_time_start = lag(trap_visit_time_restart),
+    trap_visit_time_start = case_when(
+      is.na(trap_visit_time_start) ~ as_date(trap_visit_time_end) - 1,
+      # the first day will be NA
+      as_date(trap_visit_time_end) - as_date(trap_visit_time_start) > 7 ~ as_date(trap_visit_time_end) - 1,
+      # assuming trap was stopped and we just don't have that data
+      as_date(trap_visit_time_end) - as_date(trap_visit_time_start) < 0 ~ as_date(trap_visit_time_end) - 1,
+      # this is the first day for mill
+      T ~ trap_visit_time_start
+    )
+  ) |>
+  select(
+    trap_visit_id,
+    stream,
+    site,
+    trap_name,
+    is_paper_entry,
+    trap_visit_time_start,
+    trap_visit_time_end,
+    trap_visit_time_restart,
+    fish_processed,
+    why_fish_not_processed,
+    sample_gear,
+    cone_depth,
+    trap_functioning,
+    why_trap_not_functioning,
+    trap_status_at_end,
+    total_revolutions,
+    rpm_at_start,
+    rpm_at_end,
+    in_half_cone_configuration,
+    debris_volume_gal
+  )
+
+rst_trap <- bind_rows(rst_trap, rst_trap_query_pilot_processed, edi_trap)
+
+# rst_catch ---------------------------------------------------------------
+
+# Bind rows
+rst_catch_prep <- bind_rows(rst_catch, rst_catch_query_pilot, edi_catch)
+
+# special processing for butte creek
 # find dates where the rst is fishing to use as a filter
-okie_rst <- rst_catch_prep |> 
-  filter(site == "okie dam", subsite != "okie dam fyke trap") |> 
+okie_rst <- rst_catch_prep |>
+  filter(site == "okie dam", subsite != "okie dam fyke trap") |>
   mutate(date = as_date(date)) |> # remove time so that won't affect it
   pull(date)
 
-rst_catch <- rst_catch_prep |> 
-  mutate(butte_fyke_filter = case_when(site == "okie dam" & as_date(date) %in% okie_rst ~ "rst & fyke",
-                                       site == "okie dam" & !as_date(date) %in% okie_rst ~ "fyke only",
-                                       T ~ "not butte")) |> 
+# Additional processing for rst_catch
+# If chinook salmon were not caught in the trap it will look like the trap didn't fish unless we join
+# it with trap data. This is important for ensuring hours fished is accurate and count = 0 instead of NA
+catch_dates <- rst_trap |>
+  filter(
+    visit_type %in% c("continue trapping", "end trapping", "not recorded") |
+      is.na(visit_type)
+  )  |>
+  mutate(date = case_when(
+    is.na(trap_stop_date) ~ as_date(trap_start_date),
+    T ~ as_date(trap_stop_date)
+  )) |>
+  distinct(date, stream, site, subsite)
+
+rst_catch <- full_join(catch_dates, rst_catch_raw) |>
+  mutate(
+    fork_length = ifelse(fork_length == 0, NA, fork_length),
+    # looks like there were some cases where fork length is 0. this should be handled more upstream but fixing it here for now
+    julian_week = week(date),
+    julian_year = year(date),
+    # adding week for noble
+    life_stage = ifelse(is.na(life_stage), "not recorded", life_stage),
+    subsite = case_when(
+      site == "okie dam" &
+        is.na(subsite) ~ "okie dam 1",
+      # fix missing subsites
+      is.na(subsite) ~ site,
+      subsite == "yub" ~ "hal",
+      # we made the decision to include these historical ones as halwood
+      T ~ subsite
+    ),
+    species = ifelse(
+      species %in% c("chinook", "chinook salmon"),
+      "chinook salmon",
+      species
+    ),
+    butte_fyke_filter = case_when(
+      site == "okie dam" & as_date(date) %in% okie_rst ~ "rst & fyke",
+      site == "okie dam" &
+        !as_date(date) %in% okie_rst ~ "fyke only",
+      T ~ "not butte"
+    ),
+    count = ifelse(is.na(count), 0, count)
+  ) |> # few instances where count is NA prior to joining in trap data. some of these probably should not be NA but are from old data
   # some of these happen due to EDI join and the datatackle include more species than chinook
-  filter(species %in% c("chinook","chinook salmon"), # filter for only chinook
-         life_stage != "adult",# remove the adult fish (mostly on Butte)
-         !is.na(stream),# 7 NAs I think come from knights landings
-         butte_fyke_filter != "fyke only") |> 
+  filter(
+    species == "chinook salmon",
+    # filter for only chinook
+    life_stage != "adult",
+    # remove the adult fish (mostly on Butte)
+    butte_fyke_filter != "fyke only",!is.na(stream)
+  ) |>  # 7 NAs I think come from knights landings
   select(-c(butte_fyke_filter))
 
-rst_trap <- bind_rows(rst_trap, rst_trap_query_pilot, edi_trap)  |> glimpse()
-release <- bind_rows(release_db, release_query_pilot |> 
-                       mutate(release_id = as.character(release_id)), edi_release)  |> 
+# release -----------------------------------------------------------------
+
+release <- bind_rows(release_db,
+                     release_query_pilot |>
+                       mutate(release_id = as.character(release_id)),
+                     edi_release)  |>
   filter(!is.na(number_released)) |> # there should not be any NAs
-  left_join(standard_release |> 
-              filter(!is.na(median_fork_length_released)) |> 
-              select(site, release_id, median_fork_length_released) |> 
-              distinct()) |>  # fork length is not on EDI but Josh needs it in weekly_efficiency
-  left_join(standard_release |> 
-              select(site, release_id, origin_released) |> 
+  left_join(
+    standard_release |>
+      filter(!is.na(median_fork_length_released)) |>
+      select(site, release_id, median_fork_length_released) |>
+      distinct()
+  ) |>  # fork length is not on EDI but Josh needs it in weekly_efficiency
+  left_join(standard_release |>
+              select(site, release_id, origin_released) |>
               distinct())  # fork length is not on EDI but Josh needs it in weekly_efficiency
-recaptures <- bind_rows(recaptures_db, recaptures_query_pilot |> mutate(release_id = as.character(release_id)), edi_recapture) |> glimpse()
+
+# recaptures --------------------------------------------------------------
+
+recaptures <- bind_rows(
+  recaptures_db,
+  recaptures_query_pilot |> mutate(release_id = as.character(release_id)),
+  edi_recapture
+) |> glimpse()
 
 ## SAVE TO DATA PACKAGE ---
 usethis::use_data(rst_catch, overwrite = TRUE)
 usethis::use_data(rst_trap, overwrite = TRUE)
 usethis::use_data(release, overwrite = TRUE)
 usethis::use_data(recaptures, overwrite = TRUE)
+
--- data-raw/pull_data_scripts/pull_tables_from_data_tackle_database.R
+++ data-raw/pull_data_scripts/pull_tables_from_data_tackle_database.R
@@ -48,19 +48,19 @@ try(rst_catch_query_pilot <- dbGetQuery(con, "SELECT
                                         left join mark_color mc on (em.mark_color_id  = mc.id)
                                         left join body_part bp on (em.mark_position_id  = bp.id)
                                         where (tv.program_id = 1 or tv.program_id = 2)") |> 
-  mutate(date = as.Date(trap_visit_time_end),
-         stream = tolower(stream),
-         site = ifelse(tolower(site) == "mill creek rst", "mill creek", "deer creek"), # TODO update once queries are more complicated
-         subsite = ifelse(tolower(trap_name) == "mill creek rst", "mill creek", "deer creek"),
-         site_group = stream,
-         count = num_fish_caught, 
-         run = capture_run,
-         species = tolower(species_common_name)) |> 
-    filter(is.na(release_id),
-           species == "chinook salmon") |> 
-    select(c("date", "stream", "site", "subsite", "site_group", "count", 
-             "run", "life_stage", "adipose_clipped", "dead", "fork_length", 
-             "weight", "species")))
+      mutate(date = as.Date(trap_visit_time_end),
+             stream = tolower(stream),
+             site = ifelse(tolower(site) == "mill creek rst", "mill creek", "deer creek"), # TODO update once queries are more complicated
+             subsite = ifelse(tolower(trap_name) == "mill creek rst", "mill creek", "deer creek"),
+             site_group = stream,
+             count = num_fish_caught, 
+             run = capture_run,
+             species = tolower(species_common_name)) |> 
+      filter(is.na(release_id),
+             species == "chinook salmon") |> 
+      select(c("date", "stream", "site", "subsite", "site_group", "count", 
+               "run", "life_stage", "adipose_clipped", "dead", "fork_length", 
+               "weight", "species")))
 
 # Data Checks 
 rst_catch_query_pilot$adipose_clipped |> unique() #TODO I believe these should all be FALSE 
@@ -70,28 +70,26 @@ rst_catch_query_pilot$dead |> unique() #TODO lets make sure we are defaulting to
 
 
 # Pull in Trap table 
-try(rst_trap_query_pilot <-  dbGetQuery(con, 
-                                  "SELECT 
-                                   p.program_name as program_name, 
-                                   p.stream_name as stream,
-                                   tl.trap_name as trap_name,
-                                   tl.site_name as site,
-                                   t.is_paper_entry, 
-                                   t.trap_visit_time_start, 
-                                   t.trap_visit_time_end,
-                                   fp.definition as fish_processed,
-                                   wfnp.definition as why_fish_not_processed,
-                                   t.cone_depth,
-                                   tf.definition as trap_functioning,
-                                   tsae.definition as trap_status_at_end, 
-                                   t.total_revolutions, 
-                                   t.rpm_at_start, 
-                                   t.rpm_at_end, 
-                                   t.debris_volume_gal, 
-                                   tve.measure_name, 
-                                   tve.measure_value_numeric,
-                                   tve.measure_value_text, 
-                                   tve.measure_unit
+try(rst_trap_query_pilot <-  dbGetQuery(con, "SELECT 
+                                    t.id as trap_visit_id,
+                                    p.stream_name as stream,
+                                    tl.site_name as site,
+                                    tl.trap_name as trap_name,
+                                    t.is_paper_entry, 
+                                    t.trap_visit_time_start as trap_visit_time_restart, 
+                                    t.trap_visit_time_end,
+                                    fp.definition as fish_processed,
+                                    wfnp.definition as why_fish_not_processed,
+                                    e.definition as sample_gear,
+                                    t.cone_depth,
+                                    tf.definition as trap_functioning,
+                                    wtnf.definition as why_trap_not_functioning,
+                                    tsae.definition as trap_status_at_end, 
+                                    t.total_revolutions, 
+                                    t.rpm_at_start, 
+                                    t.rpm_at_end, 
+                                    t.in_half_cone_configuration,
+                                    t.debris_volume_gal 
                                    FROM trap_visit t
                                    left join program p on (t.program_id = p.id)
                                    left join fish_processed fp on (t.fish_processed = fp.id)
@@ -99,28 +97,11 @@ try(rst_trap_query_pilot <-  dbGetQuery(con,
                                    left join trap_functionality tf on (t.trap_functioning = tf.id)
                                    left join trap_status_at_end tsae on (t.trap_status_at_end = tsae.id)
                                    left join trap_locations tl on (t.trap_location_id = tl.id) 
-                                   left join trap_visit_environmental tve on (t.id = tve.trap_visit_id)
-                                   where (t.program_id = 1 or t.program_id = 2)") |> 
-      mutate(trap_start_time = hms::as_hms(trap_visit_time_start),
-             trap_start_date = as_date(trap_visit_time_start), 
-             trap_stop_time = hms::as_hms(trap_visit_time_end),
-             trap_stop_date = as_date(trap_visit_time_end),
-             stream = tolower(stream), 
-             site = ifelse(tolower(site) == "mill creek rst", "mill creek", "deer creek"), # TODO update once queries are more complicated
-             subsite = ifelse(tolower(trap_name) == "mill creek rst", "mill creek", "deer creek"),
-             site_group = stream,
-             rpm_start = rpm_at_start, 
-             rpm_end = rpm_at_end, 
-             debris_volume = debris_volume_gal) |> # TODO confirm units match up with rst_trap data 
-      pivot_wider(names_from = measure_name, values_from = measure_value_numeric) |> 
-      rename(discharge = `flow measure`,
-             water_temp = `water temperature`) |> 
-      select(c("trap_start_date", "trap_stop_date", "stream", 
-               "site", "subsite", "site_group", "trap_functioning",  
-               "fish_processed", "rpm_start", "rpm_end", "total_revolutions", 
-               "debris_volume", "discharge", "water_temp",  "trap_start_time", "trap_stop_time")) )
+                                   left join equipment e on (t.sample_gear_id = e.id)
+                                   left join why_trap_not_functioning wtnf on (t.why_trap_not_functioning = wtnf.id)
+                                   where (t.program_id = 1 or t.program_id = 2)") )
 
-  
+
 # ENV variables we are missing 
 # turbidity, velocity - mill and deer currently do not collect. 
 # also missing visit_type but we intentionally don't collect that
@@ -158,7 +139,7 @@ try(release_query_pilot <- dbGetQuery(con, "SELECT
              life_stage = NA) |> 
       select(c("date_released", "release_id", "stream", "site", "subsite", 
                "site_group", "number_released", "run", "life_stage", "origin")) 
-             |> distinct())
+    |> distinct())
 
 # try(if(!exists("release_query_pilot"))
 #   release <- SRJPEdata::release |> filter(stream %in% c("mill creek", "deer creek"))
@@ -214,4 +195,3 @@ try(recaptures_query_pilot <- dbGetQuery(con, "SELECT
 # try(if(!exists("recaptures_query_pilot"))
 #   recaptures <- SRJPEdata::recaptures |> filter(stream %in% c("mill creek", "deer creek"))
 #   else(recaptures <- recaptures_query_pilot))
-
