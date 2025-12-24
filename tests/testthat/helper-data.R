ac73bb97f958d9916df9f14ebcc193843634de8f
alvizek <avizek@flowwest.com>
2025-12-23 16:57:35 -0800

    missing library call

--- tests/testthat/helper-data.R
+++ tests/testthat/helper-data.R
@@ -1,6 +1,6 @@
 # tests/testthat/helper-data.R
 # Loads all data files before tests run
-
+library(tidyverse)
 load("../../data/rst_catch.rda")
 load("../../data/rst_trap.rda")
 load("../../data/release.rda")
