af8e3adbbf423e17b6b9c68b3e628c5eb4b39806
alvizek <avizek@flowwest.com>
2026-01-23 11:33:23 -0800

    adds table for feather hatchery release that josh prefers and the environmental covariates

--- data-raw/pull_data_scripts/pull-cwt-release.R
+++ data-raw/pull_data_scripts/pull-cwt-release.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 
 # This table was generated in GIS and it is located in this folder: X:\GIS\Projects\028-03_SpringRunJPE_DataMgmt\APRX\cwt_data\cwt_distances
-cwt_distances <- read.csv("data-raw/cwt_data/cwt_distances_summary.csv") |>
+cwt_distances <- read_csv("data-raw/cwt_data/cwt_distances_summary.csv") |>
   add_row(Id = 8,
           to_name = "FEATHER BEL THRM HI FLOW", # this is a duplicate from feather at gridley 
           distance_rkm = cwt_distances |>
@@ -13,14 +13,14 @@ cwt_distances <- read.csv("data-raw/cwt_data/cwt_distances_summary.csv") |>
   select(release_location_name, delta_distance) |> glimpse()
 
 # This table was provided by Brett and was created to determine a lat and long for each location
-site_info <- read.csv("data-raw/cwt_data/CWT_release_sites_latlong.csv") |> 
+site_info <- read_csv("data-raw/cwt_data/CWT_release_sites_latlong.csv") |> 
   filter(hatchery_location_name == "FEATHER R HATCHERY") |> 
   left_join(cwt_distances, by = "release_location_name") |> 
   rename(release_latitude = latitude,
          release_longitude = longitude) |> glimpse()
 
-# This dataset is fro are from the Regionglimpse()# This dataset is fro are from the Regional Mark Processing Center: https://www.rmpc.org/
-cwt_data_raw <- read.csv("data-raw/cwt_data/CWT.Sac.Valley.Releases.csv") 
+# This dataset is fro are from the Regional Mark Processing Center: https://www.rmpc.org/
+cwt_data_raw <- read_csv("data-raw/cwt_data/CWT.Sac.Valley.Releases.csv") 
   
 # feather_cwt_release <- cwt_data_raw |> 
 #   filter(hatchery_location_name == "FEATHER R HATCHERY",
@@ -68,8 +68,8 @@ cwt_data_summary <- cwt_data_raw |>
   mutate(
     first_release_date = as.Date(first_release_date, format = "%m/%d/%Y"),
     last_release_date = as.Date(last_release_date, format = "%m/%d/%Y"),
-    wy_first_release = as.Date(first_release_date, format = "%m/%d/%Y"), # can use to filter for problem groups later on
-    wy_last_release = as.Date(last_release_date, format = "%m/%d/%Y"),   # can use to filter for problem groups later on
+    #wy_first_release = as.Date(first_release_date, format = "%m/%d/%Y"), # can use to filter for problem groups later on, from ashley - I don't understand these so removing
+   #wy_last_release = as.Date(last_release_date, format = "%m/%d/%Y"),   # can use to filter for problem groups later on, from ashley - I don't understand these so removing
     date_span = 1 + as.numeric(difftime(wy_last_release, wy_first_release, units = "days")), # can be used to summarize conditions over release period
     total_marked_N =  round((cwt_1st_mark_count + cwt_2nd_mark_count) * (1 - tag_loss_rate)),
     total_unmarked_N = non_cwt_1st_mark_count + non_cwt_2nd_mark_count + round((cwt_1st_mark_count + cwt_2nd_mark_count) * (tag_loss_rate)),
@@ -84,9 +84,31 @@ cwt_data_summary <- cwt_data_raw |>
   select(-c(non_cwt_1st_mark_count, non_cwt_2nd_mark_count, cwt_1st_mark_count, cwt_2nd_mark_count)) |> 
   glimpse()
 
-write.csv(cwt_data_summary, "data-raw/cwt_data/cwt_data_summary.csv")
+# Josh is currently not using this table so we are not including it in the package
+#write_csv(cwt_data_summary, "data-raw/cwt_data/cwt_data_summary.csv")
+# usethis::use_data(cwt_data_summary, overwrite = TRUE)
 
-cwt_data_grouped <- cwt_data_summary |> 
+# Josh prefers the table that is grouped by release and wanted to include environmental
+# covariates that Flora uses in survival/TT model which are the 3_category_flow_exceedance_year_type and monthly_max_flow
+exceedence_flows <- SRJPEdata::forecast_covariates |>
+  filter(name == "3_category_flow_exceedance_year_type",
+         stream == "feather river") |>
+  mutate(value = case_when(text_value == "Wet" ~ 2,
+                           text_value == "Average" ~ 1,
+                           TRUE ~ 0)) |>
+  # Flora's code calls year the same thing as water year (assumption)
+  select(year = water_year, value) |>
+  arrange(year) |> 
+  rename(exceedance_flow_year_type = value)
+
+max_flows <- SRJPEdata::forecast_covariates |>
+  filter(name == "monthly_max_flow",
+         stream == "feather river") |>
+  arrange(stream, year, month) |>
+  select(year, month, value) |> 
+  rename(monthly_max_flow = value)
+
+feather_hatchery_release <- cwt_data_summary |> 
   group_by(
     release_location_name,
     avg_weight,
@@ -95,8 +117,8 @@ cwt_data_grouped <- cwt_data_summary |>
     last_release_date,
     date_span,
     mid_release_date,
-    wy_first_release,
-    wy_last_release,
+    #wy_first_release,
+    #wy_last_release,
     delta_distance,
     release_latitude,
     release_longitude) |> 
@@ -107,8 +129,12 @@ cwt_data_grouped <- cwt_data_summary |>
     group_total_unmarked_N = sum(total_unmarked_N),
     group_total_release_N = sum(total_release_N))|> 
   mutate(
-    group_mark_rate = round(group_total_marked_N / group_total_release_N, 4)) |> 
+    group_mark_rate = round(group_total_marked_N / group_total_release_N, 4),
+    month = month(first_release_date),
+    year = ifelse(month %in% 10:12, year(first_release_date) + 1, year(first_release_date))) |> # this is water year to align with the covariates
   ungroup() |> 
-  glimpse()
+  left_join(exceedence_flows) |> 
+  left_join(max_flows)
 
-write.csv(cwt_data_grouped, "data-raw/cwt_data/cwt_data_grouped.csv")
+# write_csv(feathery_hatchery_release, "data-raw/cwt_data/cwt_data_grouped.csv")
+usethis::use_data(feather_hatchery_release, overwrite = TRUE)
