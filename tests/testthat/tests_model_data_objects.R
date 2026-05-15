7f0e6a3178330f97be6d4cfeb11725b8f27a0e4c
alvizek <avizek@flowwest.com>
2026-05-15 11:10:05 -0700

    adds tests for model data objects

--- tests/testthat/tests_model_data_objects.R
+++ tests/testthat/tests_model_data_objects.R
@@ -66,3 +66,56 @@ test_that("there is no -Inf values (hours fished...ect) when there is catch data
   expect_equal(c(FALSE, FALSE, NA), 
                nas)
 })
+
+test_that("no rows have more recaptures than releases", {
+  efficiency <- weekly_juvenile_abundance_efficiency_data |> 
+    filter(number_recaptured > number_released) 
+  erroneous_rows <- nrow(efficiency)
+  
+  expect_equal(erroneous_rows, 
+               0)
+})
+
+# check no count values for no hours_fished
+# no missing values when there is catch data (even if catch is 0)
+test_that("there are no rows where we have count > 0 when there was no effort", {
+  catch <- weekly_juvenile_abundance_catch_data |> 
+    filter(is.na(hours_fished) | hours_fished == 0)
+  count_na <- !anyNA(catch$count)
+  
+  nas = count_na
+  expect_equal(FALSE, 
+               nas)
+})
+
+# including a draft test for using simple expansion methods to check for high values. not in final form and not necessary for v1.0
+# simple expanded catch values are in reasonable ranges (not too high)
+test_that("count values are reasonable", {
+  
+  efficiency_summary <- weekly_juvenile_abundance_efficiency_data |>
+    filter(!is.na(number_recaptured),
+           !is.na(number_released)) |> 
+    mutate(simple_eff = number_recaptured / number_released) |> 
+    group_by(site) |> 
+    summarise(mean_simple_eff = mean(simple_eff)) |> 
+    ungroup()
+
+  expanded_catch <- weekly_juvenile_abundance_catch_data |>
+    filter(!is.na(count)) |> 
+    left_join(efficiency_summary, by = "site") |> 
+    mutate(simple_expansion = count / mean_simple_eff)
+    
+  expanded_catch_summary <- expanded_catch |> 
+    group_by(site) |> 
+    summarise(mean_simple_expansion = mean(simple_expansion),
+              sd_simple_expansion = sd(simple_expansion)) |> 
+    ungroup()
+  
+  expanded_catch_check <- expanded_catch |> 
+    select(site, week, run_year, simple_expansion) |> 
+    left_join(expanded_catch_summary, by = "site") |> 
+    mutate(upper_limit = mean_simple_expansion + (2 * sd_simple_expansion),
+           flag_upper = ifelse(simple_expansion > upper_limit, TRUE, FALSE))
+  
+  expect_true(all(expanded_catch_check$flag_upper))
+})
