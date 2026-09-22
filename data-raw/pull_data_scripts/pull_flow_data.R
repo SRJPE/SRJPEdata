cc106dd552e79240b9b9d81e7d00f8fe6db2534f
Erin Cain <ecain@flowwest.com>
2026-09-22 12:29:22 -0700

    fix(ci): fix error with duplicate flows by adding distinct update-type: manual

--- data-raw/pull_data_scripts/pull_flow_data.R
+++ data-raw/pull_data_scripts/pull_flow_data.R
@@ -397,7 +397,8 @@ flow_daily <- data.table::rbindlist(
   use.names = TRUE,
   fill = TRUE
 ) |> 
-  dplyr::filter(lubridate::year(date) > 1990) 
+  dplyr::filter(lubridate::year(date) > 1990) |> 
+  distinct(date, stream, site_group, gage_agency, gage_number, parameter, statistic) # ADDS distinct call given duplicates exist for flow data pulls from CDEC 
 
 # Check to make sure there are no duplicates because the reshaping with result in values of 0 and 1 if duplicates exist which is a major issue.
 find_duplicates <- flow_daily |> 
